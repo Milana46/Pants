2b55fe844b61babc8682e9ddf7a62de172502e5c
Milana46 <bogatyrevamilana462004@gmail.com>
2024-10-19 20:28:06 +0300

    PANTS-5

--- app/src/main/java/com/example/pants/data/repository/ColorRepositoryImpl.kt
+++ app/src/main/java/com/example/pants/data/repository/ColorRepositoryImpl.kt
@@ -4,11 +4,13 @@ import com.example.pants.data.mapper.toColorModel
 import com.example.pants.domain.model.ColorModel
 import com.example.pants.data.repository.utils.generateRandomColor
 import com.example.pants.data.service.ColorApiService
+import com.example.pants.domain.repository.COMMON_USE_NAMES
 import com.example.pants.domain.repository.ColorRepository
 import java.util.Locale
 
 class ColorRepositoryImpl(
     private val apiService: ColorApiService,
+
 ) : ColorRepository {
 
     override suspend fun getRandomColors(count: Int): Result<Set<ColorModel>> = runCatching {
@@ -34,38 +36,7 @@ class ColorRepositoryImpl(
 //        return List(size) { generateRandomColor() }
 //    }
 
-    private companion object {
-        val COMMON_USE_NAMES = setOf(
-            "beige",
-            "black",
-            "blue violet",
-            "blue",
-            "brown",
-            "crimson",
-            "cyan",
-            "gold",
-            "gray",
-            "green",
-            "indigo",
-            "khaki",
-            "lavender",
-            "lime green",
-            "magenta",
-            "maroon",
-            "navy blue",
-            "olive",
-            "orange",
-            "pink",
-            "plum",
-            "purple",
-            "red",
-            "salmon",
-            "silver",
-            "sky blue",
-            "teal",
-            "violet",
-            "white",
-            "yellow",
-        )
-    }
+
+
+
 }
