77e603ab9dc7d51de8c230b75b513d4ca795fe02
Milana46 <bogatyrevamilana462004@gmail.com>
2024-10-19 20:28:06 +0300

    PANTS-5

--- app/src/main/java/com/example/pants/domain/repository/ColorRepository.kt
+++ app/src/main/java/com/example/pants/domain/repository/ColorRepository.kt
@@ -2,7 +2,44 @@ package com.example.pants.domain.repository
 
 import com.example.pants.domain.model.ColorModel
 
+
+
+val COMMON_USE_NAMES = setOf(
+    "beige",
+    "black",
+    "blue violet",
+    "blue",
+    "brown",
+    "crimson",
+    "cyan",
+    "gold",
+    "gray",
+    "green",
+    "indigo",
+    "khaki",
+    "lavender",
+    "lime green",
+    "magenta",
+    "maroon",
+    "navy blue",
+    "olive",
+    "orange",
+    "pink",
+    "plum",
+    "purple",
+    "red",
+    "salmon",
+    "silver",
+    "sky blue",
+    "teal",
+    "violet",
+    "white",
+    "yellow",
+)
+
 interface ColorRepository {
+
+
 
     suspend fun getRandomColors(count: Int): Result<Set<ColorModel>>
 }
